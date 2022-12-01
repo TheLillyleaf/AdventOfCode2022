9fa7e820f783c94297e45c12e69a9bb39e51902d
toli2003 <toli2003@student.miun.se>
2022-12-01 20:33:37 +0100

    clean code

--- index.ts
+++ index.ts
@@ -1,27 +1,20 @@
 import * as fs from "fs"
 
-//Day 1 a
-
-const day1a = fs
+const input = fs
 	.readFileSync("./input")
 	.toString()
 	.split("\n\n")
 	.map((string: string) => string.split("\n"))
 	.map(line => line.map(line => +line))
 	.map(line => line.reduce((init, curr) => init + curr))
-	.reduce((first, curr) => Math.max(first, curr))
+//Day 1 a
+
+const day1a = input.reduce((first, curr) => Math.max(first, curr))
+const day1a2 = Math.max(...input)
 
 console.log(`Day 1 result: ${day1a}`)
-
+console.log(`Day 1 result:${day1a2}`)
 // Day1 B
-const day1b = fs
-	.readFileSync("./dec1/input")
-	.toString()
-	.split("\n\n")
-	.map((string: string) => string.split("\n"))
-	.map(line => line.map(line => +line))
-	.map(line => line.reduce((init, curr) => init + curr))
-	.sort()
-	.reverse()
+const day1b = input.sort().reverse()
 
 console.log(`Day 2 result: ${day1b[0] + day1b[1] + day1b[2]}`)
