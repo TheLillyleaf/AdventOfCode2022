9328f2cb15502ca98d6b3d8dc506a2dc3472bcd4
toli2003 <toli2003@student.miun.se>
2022-12-06 07:48:35 +0100

    Day 5

--- index.ts
+++ index.ts
@@ -1,40 +1,52 @@
-import * as fs from "fs"
+import { readFileSync } from "fs"
+import { resolve } from "path"
 
-const day4A = fs
-	.readFileSync("./input")
+type S<T> = Array<Array<T>>
+type F = (c: S<string>, l: S<number>) => S<string>
+
+const [x, y] = readFileSync(resolve("./input"))
 	.toString()
-	.trim()
-	.split("\n")
-	.map(line => line.split(",").map(section => section.split("-").map(pos => +pos)))
-	.reduce((accumulator, currentArray) => {
-		if (currentArray[0][0] <= currentArray[1][0] && currentArray[0][1] >= currentArray[1][1]) {
-			accumulator++
-		} else if (currentArray[0][0] >= currentArray[1][0] && currentArray[0][1] <= currentArray[1][1]) {
-			accumulator++
-		}
-		return accumulator
-	}, 0)
+	.split("\n\n")
+	.map(v => v.split("\n"))
+const j = y.map(v =>
+	v
+		.split(" ")
+		.map(w => parseInt(w, 10))
+		.filter(Boolean)
+)
+const s = x.map(v =>
+	v
+		.replace(/[\[\]]/g, " ")
+		.split("")
+		.map(v => v.trim())
+)
+const c = s
+	.pop()
+	?.map(
+		(v, i) =>
+			v &&
+			s
+				.map(w => w[i])
+				.filter(Boolean)
+				.reverse()
+	)
+	.filter(v => v.length) as S<string>
 
-console.log(`Day 4A result is:  ${day4A}`)
+const a: F = (c, l) =>
+	l.reduce(
+		(v, [m, f, t]) =>
+			Array(m)
+				.fill(0)
+				.reduce((z: S<string>) => z.map((h, i) => (i === t - 1 ? [...h, v[f - 1].pop()] : h)), v),
+		c
+	)
+const b: F = (c, l) =>
+	l.reduce(
+		(v, [m, f, t]) =>
+			v.map((h, i) => (i === t - 1 ? [...h, ...v[f - 1].splice(v[f - 1].length - m, v[f - 1].length)] : h)),
+		c
+	)
+const r = (f: F, c: S<string>, j: S<number>): string =>
+	f(JSON.parse(JSON.stringify(c)), j).reduce((t, v) => t + v.pop(), "")
 
-const day4b = fs
-	.readFileSync("./input")
-	.toString()
-	.trim()
-	.split("\n")
-	.map(line => line.split(",").map(section => section.split("-").map(pos => +pos)))
-	.reduce((accumulator, currentArray) => {
-		const firstMin = currentArray[0][0]
-		const firstMax = currentArray[0][1]
-		const secondMin = currentArray[1][0]
-		const secondMax = currentArray[1][1]
-
-		if (firstMin <= secondMin && firstMax >= secondMin) {
-			accumulator++
-		} else if (firstMin >= secondMin && firstMin <= secondMax) {
-			accumulator++
-		}
-		return accumulator
-	}, 0)
-
-console.log(`Day 4B result is: ${day4b} `)
+console.debug(r(a, c, j), r(b, c, j))
