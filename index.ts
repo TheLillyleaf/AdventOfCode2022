dbff12b76e3eb9860b5baa2c72e70d0f90b05515
toli2003 <toli2003@student.miun.se>
2022-12-06 08:16:17 +0100

    Day 6 done

--- index.ts
+++ index.ts
@@ -1,52 +1,37 @@
-import { readFileSync } from "fs"
-import { resolve } from "path"
+import * as fs from "fs"
 
-type S<T> = Array<Array<T>>
-type F = (c: S<string>, l: S<number>) => S<string>
-
-const [x, y] = readFileSync(resolve("./input"))
+const day6A = fs
+	.readFileSync("./input")
 	.toString()
-	.split("\n\n")
-	.map(v => v.split("\n"))
-const j = y.map(v =>
-	v
-		.split(" ")
-		.map(w => parseInt(w, 10))
-		.filter(Boolean)
-)
-const s = x.map(v =>
-	v
-		.replace(/[\[\]]/g, " ")
-		.split("")
-		.map(v => v.trim())
-)
-const c = s
-	.pop()
-	?.map(
-		(v, i) =>
-			v &&
-			s
-				.map(w => w[i])
-				.filter(Boolean)
-				.reverse()
-	)
-	.filter(v => v.length) as S<string>
+	.trim()
+	.split("")
+	.reduce((result, curr, i, day1b) => {
+		const temp = day1b.slice(i, i + 4)
+		const s = new Set()
 
-const a: F = (c, l) =>
-	l.reduce(
-		(v, [m, f, t]) =>
-			Array(m)
-				.fill(0)
-				.reduce((z: S<string>) => z.map((h, i) => (i === t - 1 ? [...h, v[f - 1].pop()] : h)), v),
-		c
-	)
-const b: F = (c, l) =>
-	l.reduce(
-		(v, [m, f, t]) =>
-			v.map((h, i) => (i === t - 1 ? [...h, ...v[f - 1].splice(v[f - 1].length - m, v[f - 1].length)] : h)),
-		c
-	)
-const r = (f: F, c: S<string>, j: S<number>): string =>
-	f(JSON.parse(JSON.stringify(c)), j).reduce((t, v) => t + v.pop(), "")
+		temp.forEach(element => s.add(element))
 
-console.debug(r(a, c, j), r(b, c, j))
+		if (s.size == temp.length && result <= 0) {
+			return result + (i + 4)
+		}
+		return result
+	}, 0)
+const day6B = fs
+	.readFileSync("./input")
+	.toString()
+	.trim()
+	.split("")
+	.reduce((result, curr, i, day1b) => {
+		const temp = day1b.slice(i, i + 14)
+		const s = new Set()
+
+		temp.forEach(element => s.add(element))
+
+		if (s.size == temp.length && result <= 0) {
+			return result + (i + 14)
+		}
+		return result
+	}, 0)
+
+console.log(`Day 6 A result is: ${day6A}`)
+console.log(`Day 6 B result is: ${day6B}`)
